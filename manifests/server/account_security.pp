6b407d8574778a681b4b53a4cbfe969107544006
Nino Paolo <o70vd@yahoo.com>
2013-12-07 03:34:09 +0800

    Fixed mysql root duplication error.

--- manifests/server/account_security.pp
+++ manifests/server/account_security.pp
@@ -1,14 +1,23 @@
 class mysql::server::account_security {
+
   mysql_user {
-    [ "root@${::fqdn}",
-      'root@127.0.0.1',
+    [ 'root@127.0.0.1',
       'root@::1',
       "@${::fqdn}",
-      '@localhost',
       '@%']:
     ensure  => 'absent',
     require => Anchor['mysql::server::end'],
   }
+
+  if ($::fqdn != 'localhost') {
+    mysql_user {
+      [ "root@${::fqdn}",
+        '@localhost']:
+      ensure  => 'absent',
+      require => Anchor['mysql::server::end'],
+    }
+  }
+
   if ($::fqdn != $::hostname) {
     mysql_user { ["root@${::hostname}", "@${::hostname}"]:
       ensure  => 'absent',
